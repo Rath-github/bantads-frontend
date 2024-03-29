99c0181adc9d9961e8ef7074e778f62a6f6dcda0
KenjiKuroda <103075517+KenjiKuroda@users.noreply.github.com>
2024-03-29 16:18:43 -0300

    feat: Adicionado função de máscaras em app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxMaskModule } from 'ngx-mask';
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -13,6 +16,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule
+    NgxMaskModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
