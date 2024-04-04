30fbcb1790b54d2d2b6c7606a9f4afac1e16a1dc
KenjiKuroda <103075517+KenjiKuroda@users.noreply.github.com>
2024-04-04 17:07:09 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ import { NgxMaskModule } from 'ngx-mask';
     BrowserModule,
     FormsModule,
     AppRoutingModule
+    HttpClientModule
     NgxMaskModule.forRoot()
   ],
   providers: [],
