c648d7c786acd76712f31655eafe19b73dae54ee
KenjiKuroda <103075517+KenjiKuroda@users.noreply.github.com>
2024-04-04 18:05:15 -0300

    feat: Update app.component.ts para conexão json server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DadosService } from 'caminho-para-o-servico/dados.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'bantads';
+  clientes: any[];
+  gerentes: any[];
+
+  constructor(private dadosService: DadosService) { }
+
+  ngOnInit(): void {
+    this.dadosService.getClientes().subscribe(data => {
+      this.clientes = data.clientes;
+    });
+
+    this.dadosService.getGerentes().subscribe(data => {
+      this.gerentes = data.gerentes;
+    });
+  }
 }
