af63c48604ef57c83fc5be02f0797bf72e490d58
Lucas Dias <lucasdias789@gmail.com>
2024-04-08 14:43:28 -0300

    styles: tela inicial e tabela consulta cliente

--- src/app/gerente/consultar-cliente/consultar-cliente.component.ts
+++ src/app/gerente/consultar-cliente/consultar-cliente.component.ts
@@ -4,6 +4,9 @@ interface DadosCliente {
   cpf: string;
   nome: string;
   salario: number;
+  cidade: string;
+  estado: string;
+  saldoConta: number;
 }
 
 @Component({
@@ -12,12 +15,12 @@ interface DadosCliente {
   styleUrls: ['./consultar-cliente.component.css']
 })
 export class ConsultarClienteComponent {
-  clienteGerente: DadosCliente[] = [
-    { cpf: '11111111111', nome: 'Lucas Dias', salario: 10000 },
-    { cpf: '22222222222', nome: 'Allan Neves', salario: 8000 },
-    { cpf: '33333333333', nome: 'Henrique Prokopenko', salario: 6000 },
-    { cpf: '44444444444', nome: 'Marcos Moreira', salario: 2000 },
-    { cpf: '55555555555', nome: 'Caique', salario: 500 },
+  clientes: DadosCliente[] = [
+    { cpf: '11111111111', nome: 'Lucas Dias', salario: 100000, cidade: 'Curitiba', estado: 'Paraná', saldoConta: 10000000 },
+    { cpf: '22222222222', nome: 'Allan Neves', salario: 50000, cidade: 'Curitiba', estado: 'Paraná', saldoConta: 800000 },
+    { cpf: '33333333333', nome: 'Henrique Prokopenko', salario: 3000, cidade: 'Curitiba', estado: 'Paraná', saldoConta: 6000 },
+    { cpf: '44444444444', nome: 'Marcos Moreira Gomes', salario: 4000, cidade: 'Montes Claros', estado: 'Minas Gerais', saldoConta: 2000 },
+    { cpf: '55555555555', nome: 'Caique', salario: 500, cidade: 'Campo Grande', estado: 'Mato Grosso do Sul', saldoConta: 7000 },
   ];
 
   formatarCPF(cpf: string): string {
@@ -32,6 +35,6 @@ export class ConsultarClienteComponent {
 
   consultarCliente() {
     // Encontra o cliente pelo CPF consultado se n mostra a msg "Cliente não encontrado"
-    this.clienteFiltrado = this.clienteGerente.find(cliente => cliente.cpf === this.cpfConsultado);
+    this.clienteFiltrado = this.clientes.find(cliente => cliente.cpf === this.cpfConsultado);
   }
 }
--- src/app/gerente/tela-inicial-gerente/tela-inicial-gerente.component.ts
+++ src/app/gerente/tela-inicial-gerente/tela-inicial-gerente.component.ts
@@ -4,6 +4,9 @@ interface DadosCliente {
   cpf: string;
   nome: string;
   salario: number;
+  cidade: string;
+  estado: string;
+  saldoConta: number;
 }
 
 @Component({
@@ -14,12 +17,16 @@ interface DadosCliente {
 export class TelaInicialGerenteComponent {
 
   pedidosAutocadastro: DadosCliente[] = [
-    { cpf: '111.111.111-00', nome: 'test 1000', salario: 1000 },
-    { cpf: '111.111.111-00', nome: 'test 5000', salario: 5000 },
-    { cpf: '111.111.111-00', nome: 'test 3000', salario: 3000 },
-    { cpf: '111.111.111-00', nome: 'test 4000', salario: 4000 },
-    { cpf: '111.111.111-00', nome: 'test 600', salario: 600 },
+    { cpf: '11111111111', nome: 'teste 1000', salario: 1000, cidade: 'Curitiba', estado: 'Paraná', saldoConta: 2000 },
+    { cpf: '22222222222', nome: 'teste 2000', salario: 2000, cidade: 'Curitiba', estado: 'Paraná', saldoConta: 4000 },
+    { cpf: '33333333333', nome: 'teste 3000', salario: 3000, cidade: 'Curitiba', estado: 'Paraná', saldoConta: 6000 },
+    { cpf: '44444444444', nome: 'teste 4000', salario: 4000, cidade: 'Montes Claros', estado: 'Minas Gerais', saldoConta: 8000 },
+    { cpf: '55555555555', nome: 'teste 5000', salario: 5000, cidade: 'Campo Grande', estado: 'Mato Grosso do Sul', saldoConta: 10000 },
   ];
+
+  formatarCPF(cpf: string): string {
+    return cpf.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})$/, '$1.$2.$3-$4');
+}
 
   Aprovar() {
     alert("Cadastro aprovado!");
